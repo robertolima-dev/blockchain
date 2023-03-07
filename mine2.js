c44c7d44708a34631d0d29ae040f1262086afe29
Roberto Lima <44699957+robertolima-dev@users.noreply.github.com>
2023-03-07 20:18:36 -0300

    Update mine2.js

--- mine2.js
+++ mine2.js
@@ -7,7 +7,7 @@ async function addNewBloc() {
                 user: 'Sou um user teste 4', 
                 value: `${Math.floor(Math.random()*(999-100+1)+100)}U$`,
                 img: 'base64...',
-                news: 'O presidente Jair Bolsonaro é genocida'
+                news: 'Aqui recebemos outro dado'
             },
         ]}
         console.log(`**************** Início do bloco ${i} ****************`)
